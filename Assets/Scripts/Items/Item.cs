dadf5906c31d22f89531fee62f5766f1e262c977
Yousif <yel.wishahy@gmail.com>
2023-01-21 19:21:38 -0800

    initial commit with item, game controller, rat casses

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private bool pickedUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickedUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.E) && pickedUp)
+        //     Drop();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("trigger");
+        if (!pickedUp && other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            var p = other.GetComponent<Player>();
+            p.PickupItem(this);
+            pickedUp = true;
+        }
+    }
+
+    void Drop()
+    {
+        transform.parent = null;
+    }
+}
