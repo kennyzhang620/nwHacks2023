bb6a338565c3b05e2bd36264a68b639a41d2397e
KTZHANG <kennyz620@hotmail.com>
2023-01-22 06:36:27 -0800

    Upload test

--- Assets/VideoCaptureSol.cs
+++ Assets/VideoCaptureSol.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using System.Collections;
 using System.Linq;
 using UnityEngine.Windows.WebCam;
+using UnityEngine.Networking;
+using System.Text;
+using TinyJson;
 
 public class VideoCaptureSol : MonoBehaviour
 {
@@ -11,6 +14,58 @@ public class VideoCaptureSol : MonoBehaviour
     float m_stopRecordingTimer = float.MaxValue;
 
     public string UploadFilePath;
+    
+    IEnumerator Upload(string filename)
+    {
+        WWWForm webForm = new WWWForm();
+
+        print("Uploading...");
+
+        // convert our class to json
+        string JsonData = "{\"name\": \"test\"}";
+            
+
+        // instance of unity web request
+        UnityWebRequest www = UnityWebRequest.Post("https://livepeer.studio/api/asset/request-upload", "POST");
+
+        // setup upload/download headers (this is what sets the json body)
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(JsonData);
+        www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+        // set your headers
+        www.SetRequestHeader("Authorization", "Bearer " + "0488e0b2-7284-42cd-ad30-64a49b924d6c");
+        www.SetRequestHeader("Content-Type", "application/json");
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            print(www.error);
+        }
+        else
+        {
+            print("---> " + www.downloadHandler.text);
+            var json = www.downloadHandler.text;
+            UploadObj tx = JSONParser.FromJson<UploadObj>(json);
+
+            print("url " + tx.url);
+
+            UnityWebRequest www2 = UnityWebRequest.Put(tx.url, "PUT");
+
+            // setup upload/download headers (this is what sets the json body)
+            byte[] bodyRaw2 = Encoding.UTF8.GetBytes(filename);
+            www2.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw2);
+            www2.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+            // set your headers
+            www2.SetRequestHeader("Authorization", "Bearer " + "0488e0b2-7284-42cd-ad30-64a49b924d6c");
+            www2.SetRequestHeader("Content-Type", "video/mp4");
+
+            yield return www2.SendWebRequest();
+        }
+    }
+
 
     // Use this for initialization
     void Start()
@@ -80,7 +135,7 @@ public class VideoCaptureSol : MonoBehaviour
     {
         Debug.Log("Stopped Video Capture Mode!");
 
-
+        StartCoroutine(Upload(UploadFilePath));
     }
 
     void OnStartedRecordingVideo(VideoCapture.VideoCaptureResult result)
